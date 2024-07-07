2d00c6ca36cb080b944939eaf17e77ae4684ca59
Krzysztof Hager <hager.krzysztof.hk@gmail.com>
2024-07-07 19:49:24 +0200

    Obsługa odczytywania zawartości plików json, xml, yaml i konwersja do dict

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 """Program do konwersji danych w formatach .xml, .json, .yml."""
 import argparse
+import dicttoxml
+import xmltodict
+import json
+import yaml
 
 def arguments():
     """Funkcja obsługująca parsowanie argumentów przy uruchomieniu programu."""
@@ -21,9 +25,23 @@ def arguments():
     argumenty = parser.parse_args()
     return argumenty
 
+def parse_data(source_file):
+    """Funkcja do odczytywania danych z plików i konwersji do słownika."""
+    if source_file.name[-4:] == 'json':
+        content_dict = json.loads(source_file.read())
+    elif source_file.name[-3:] == 'xml':
+        content_dict = xmltodict.parse(source_file.read())
+    elif source_file.name[-3:] == 'yml':
+        content_dict = yaml.load(source_file.read(), Loader=yaml.SafeLoader)[0]
+    else:
+        print("Nieobsługiwany format pliku źródłowego.")
+        return 0
+    return content_dict
+
 def main():
     """Główna funkcja programu."""
-    print(arguments().source_file.name)
+    argumenty = arguments()
+    parse_data(argumenty.source_file)
     return 0
 
 if __name__ == "__main__":
